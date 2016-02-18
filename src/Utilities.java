4a203897d22180148559c6fbb40619f9c2d61405
jfortich <jfortich@uci.edu>
2016-02-17 18:02:23 -0800

    Everything

---
+++ src/Utilities.java
@@ -0,0 +1,112 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * A collection of utility methods for text processing.
+ */
+public class Utilities {
+	/**
+	 * Reads the input text file and splits it into alphanumeric tokens.
+	 * Returns an ArrayList of these tokens, ordered according to their
+	 * occurrence in the original text file.
+	 * 
+	 * Non-alphanumeric characters delineate tokens, and are discarded.
+	 *
+	 * Words are also normalized to lower case. 
+	 * 
+	 * Example:
+	 * 
+	 * Given this input string
+	 * "An input string, this is! (or is it?)"
+	 * 
+	 * The output list of strings should be
+	 * ["an", "input", "string", "this", "is", "or", "is", "it"]
+	 * 
+	 * @param input The file to read in and tokenize.
+	 * @return The list of tokens (words) from the input file, ordered by occurrence.
+	 */
+	public static ArrayList<String> tokenizeFile(String input) {
+		// Normalizes the string "tokens" to all lowercase
+		input = input.toLowerCase();
+
+		// creates a new ArrayList using the String tokens,
+		// splits the string into alphanumeric characters only
+		ArrayList<String> tokenList = new ArrayList<String>(Arrays.asList(input.split("\\W+")));
+		return tokenList;
+	}
+	
+	/**
+	 * Takes a list of {@link Frequency}s and prints it to standard out. It also
+	 * prints out the total number of items, and the total number of unique items.
+	 * 
+	 * Example one:
+	 * 
+	 * Given the input list of word frequencies
+	 * ["sentence:2", "the:1", "this:1", "repeats:1",  "word:1"]
+	 * 
+	 * The following should be printed to standard out
+	 * 
+	 * Total item count: 6
+	 * Unique item count: 5
+	 * 
+	 * sentence	2
+	 * the		1
+	 * this		1
+	 * repeats	1
+	 * word		1
+	 * 
+	 * 
+	 * Example two:
+	 * 
+	 * Given the input list of 2-gram frequencies
+	 * ["you think:2", "how you:1", "know how:1", "think you:1", "you know:1"]
+	 * 
+	 * The following should be printed to standard out
+	 * 
+	 * Total 2-gram count: 6
+	 * Unique 2-gram count: 5
+	 * 
+	 * you think	2
+	 * how you		1
+	 * know how		1
+	 * think you	1
+	 * you know		1
+	 * 
+	 * @param frequencies A list of frequencies.
+	 */
+	public static void printFrequencies(List<Frequency> frequencies) {
+		// Calculates total frequency count and unique count
+		int totalCount  = 0;
+		int uniqueCount = 0;
+		for (Frequency f : frequencies) {
+			totalCount = totalCount + f.getFrequency();
+			uniqueCount++;
+		}
+		
+		// Gets first item in list to check for 2-gram frequencies:
+		// 	Splits the text at the space; if the length of the resulting
+		//  array is 2, the text is a 2-gram frequency; Otherwise
+		//  the text is a regular frequency
+		String text = frequencies.get(0).getText();
+		// Prints correct format if list contains 2-gram frequencies
+		if ((text.split("\\s+")).length == 2) {
+			System.out.print("Total 2-gram count: " + Integer.toString(totalCount) + "\n");
+			System.out.print("Unique 2-gram count: " + Integer.toString(uniqueCount) + "\n\n");
+			for (Frequency freq : frequencies) {
+				System.out.printf("%-15s %d %n",freq.getText(),freq.getFrequency());
+			}
+		}
+		// Else prints format for regular frequencies
+		else {
+			System.out.print("Total item count: " + Integer.toString(totalCount) + "\n");
+			System.out.print("Unique item count: " + Integer.toString(uniqueCount) + "\n\n");
+			for (Frequency freq : frequencies) {
+				System.out.printf("%-20s %d %n",freq.getText(),freq.getFrequency());
+			}
+		}
+	}
+
+}
---
+++ src/indexer.java
@@ -0,0 +1,116 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import org.jsoup.Jsoup;
+
+import com.google.common.collect.ImmutableList;
+
+/**
+ * INF 141 Information Retrieval
+ * Ricky Fong 			# 821869
+ * Jasmine Fortich		# 46446130
+ * Natalie Kassir		# 14591873
+ **/
+
+public class indexer {
+	
+	// List of stop words
+	public static final ImmutableList<String> STOP_WORDS = ImmutableList.of("a", "about", "above", "after", "again", 
+	                                              "against", "all", "am", "an", "and", "any", "are",
+	                                              "arent", "as", "at", "at", "be", "because", "been",
+	                                              "before", "being", "below", "between", "both", "but",
+	                                              "by", "cant", "cannot", "could", "couldnt", "did", "didnt",
+	                                              "do", "does", "doesnt", "doing", "each", "few", "for",
+	                                              "from", "further", "had", "hadnt", "has", "hasnt", "have",
+	                                              "havent", "having", "he", "hed", "hell", "hes", "her", "here",
+	                                              "heres", "hers", "herself", "him", "himself", "his", "how",
+	                                              "hows", "i", "id", "ill", "im", "ive", "if", "in", "into",
+	                                              "is", "isnt", "it", "its", "itself", "lets", "me", "more",
+	                                              "most", "mustnt", "my", "myself", "no", "nor", "not", "of",
+	                                              "off", "on", "once", "only", "or", "other", "ought", "our",
+	                                              "ours", "ourselves", "out", "over", "own", "same", "shant",
+	                                              "she", "shed", "shell", "shes", "shes", "shouldnt", "so", "some",
+	                                              "such", "than", "that", "thats", "the", "their", "theres", 
+	                                              "theirs", "them", "themselves", "then", "there", "theres",
+	                                              "these", " they", "theyd", "theyll", "theyre", "theyve", "this",
+	                                              "those", "through", "to", "too", "under", "until", "up", "very",
+	                                              "was", "wasnt", "we", "wed", "well", "were", "weve", "werent",
+	                                              "what", "whats", "when", "whens", "where", "wheres", "which",
+	                                              "while", "whos", "who", "whom", "why", "whys", "with", "wont",
+	                                              "would", "wouldnt", "you", "youd", "youll", "youre", "youve",
+	                                              "your", "yours", "yourself", "yourselves");
+	
+	static HashMap<String, Integer> term2termid = new HashMap<String, Integer>();
+	static HashMap<String, Integer> docID = new HashMap<String, Integer>();
+	static HashMap<Integer, ArrayList> docid2termlist = new HashMap<Integer, ArrayList>();
+	static HashMap<Integer, String> termid2term = new HashMap<Integer, String>();
+	static int termID = 0;
+	
+	// Processes html file and gets content and tokens
+	public static void processPage (File child) {
+		String content = "";
+		try {
+	        BufferedReader in = new BufferedReader(new FileReader(child));
+	        String str;
+	        
+	        while ((str = in.readLine()) != null) {
+	            content += str;
+	        }
+	        in.close();
+	        
+	        String parsedHTML = Jsoup.parse(content).text();
+	        ArrayList<String> tokens = Utilities.tokenizeFile(parsedHTML);
+	        termID =  termID + 1;
+	        
+	        for (String token : tokens) {
+	        	term2termid.put(token, termID);
+	        }
+	        
+	        
+		    System.out.print("Content: " + parsedHTML + "\n");
+		    System.out.print("Tokens: " + Utilities.tokenizeFile(parsedHTML) + "\n");
+		    System.out.print(term2termid);
+		    
+		  
+		}
+	    catch (IOException e) {
+		e.printStackTrace();
+	    }
+	}
+	
+	
+	public static void main(String[] args) throws Exception {
+	    String file = "C:\\Users\\Jasmine\\Documents\\Search Engine Data\\Html\\access.ics.uci.educontact.html";
+	    String directory = "C:\\Users\\Jasmine\\Documents\\Search Engine Data\\smallTest";
+	    
+	    File dir = new File(directory);
+	    File[] directoryListing = dir.listFiles();
+	    String list = Arrays.toString(directoryListing);
+	    System.out.print(list + "\n\n");
+	    
+	    
+	    // Solution: https://stackoverflow.com/questions/4917326/how-to-iterate-over-the-files-of-a-certain-directory-in-java
+	    if (directoryListing != null) {
+	    	for (File child : directoryListing ) {
+	    		System.out.print(child + "\n\n");
+	    		processPage(child);
+	    	}
+	    	
+	      } else {
+	        // Handle the case where dir is not really a directory.
+	        // Checking dir.isDirectory() above would not be sufficient
+	        // to avoid race conditions with another process that deletes
+	        // directories.
+	    	System.out.print("else happened");
+	      }	      
+
+	}
+}
+
